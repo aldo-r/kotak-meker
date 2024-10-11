41210e459d5f0ffb2dddf949b813a6cf14b5feac
aldo-r <113779949+aldo-r@users.noreply.github.com>
2024-10-11 16:20:01 +0700

    ui update

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-function getValues() {
-    let width = parseInt(document.getElementById('width').value);
-    let height = parseInt(document.getElementById('height').value);
+document.addEventListener('DOMContentLoaded', function () {
+    function getValues() {
+        let width = parseInt(document.getElementById('width').value);
+        let height = parseInt(document.getElementById('height').value);
 
-    let lebar = `w-[${width}px]`;
-    let tinggi = `h-[${height}px]`;
+        let lebar = `w-[${width}px]`;
+        let tinggi = `h-[${height}px]`;
 
-    let templet = [lebar, tinggi, 'shadow-xl', 'border-2', 'border-black'];
+        let templet = [lebar, tinggi, 'shadow-xl', 'border-2', 'border-black'];
 
-    let kotak = document.getElementById('kotak');
-    templet.forEach(cls => kotak.classList.add(cls));
+        let kotak = document.getElementById('kotak');
+        templet.forEach(cls => kotak.classList.add(cls));
 
-    console.log(`Classes added: ${templet.join(' ')}`);
-}
+        let text = `Classes added: ${templet.join(' ')}`
 
+        let code = document.getElementById('code').innerHTML = text
+    }
+
+    document.querySelector('button').onclick = getValues;
+});
