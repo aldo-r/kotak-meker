55f16c0229097aa3963e5fd3e58104012d90e3e3
aldo-r <113779949+aldo-r@users.noreply.github.com>
2024-10-14 15:01:20 +0700

    js update

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     function getValues() {
-        let width = parseInt(document.getElementById('width').value);
-        let height = parseInt(document.getElementById('height').value);
+        let width = parseInt($('#width').val());
+        let height = parseInt($('#height').val());
 
         let lebar = `w-[${width}px]`;
         let tinggi = `h-[${height}px]`;
 
-        let templet = [lebar, tinggi, 'shadow-xl', 'border-2', 'border-black'];
+        let templet = [lebar, tinggi, 'shadow-xl', 'border-2', 'border-black', 'bg-white'];
+        let kotak = $('#kotak');
 
-        let kotak = document.getElementById('kotak');
-        templet.forEach(cls => kotak.classList.add(cls));
+        $.each(templet, function (index, cls) {
+            kotak.addClass(cls);
+        });
 
-        let text = `Classes added: ${templet.join(' ')}`
-
-        let code = document.getElementById('code').innerHTML = text
+        let text = `Classes added: ${templet.join(' ')}`;
+        $('#code').html(text);
     }
 
     document.querySelector('button').onclick = getValues;
 });
+
